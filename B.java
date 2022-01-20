215736e572d4fa41fc8f233aa6605942a92aa8b5
crows1 <96900749+crows1@users.noreply.github.com>
2022-01-21 00:19:52 +0900

    Add files via upload

---
+++ B.java
@@ -0,0 +1,15 @@
+class B {
+	/**
+        생성자입니다
+	*/
+	B(){
+		System.out.println("1");
+		/*System.out.println("2");
+		System.out.println("3");
+		*/
+	}
+	public static void main(String[] args) 
+	{
+		new B();
+	}
+}
